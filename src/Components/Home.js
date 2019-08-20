4cf420af1b805fa354c002ba26b3fce99481e9dc
aliLorentz <ajosue998@gmail.com>
2019-08-19 20:10:33 -0430

    pre-terminando productos

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
+
 import NavBar from './NavBar.js';
+import Productos from '../Components/Productos.js';
+
 import "../Styles/Home.css";
+
 import bannerPrincipal from "../imagen/banner - principal.png";
 import bannSec1 from "../imagen/1.png";
 import bannSec2 from "../imagen/2.png";
@@ -10,6 +14,7 @@ export default class Home extends React.Component {
 		return (
 			<div className="home">
 				<NavBar/>
+
 				<div className="banner-img">
 					<div className="principal">
 						<img src={bannerPrincipal} alt="bannerPrincipal"/>
@@ -17,14 +22,16 @@ export default class Home extends React.Component {
 					<div className="secundario">
 						<div className="part1">
 							<h2>Nuestra gama de productos</h2>
-							<img src={bannSec1} alt="bannerPrincipal"/>
+							<img src={bannSec1} alt="bannerSecundario"/>
 						</div>
 						<div className="part2">
 							<h2>Nuestro alojamiento creo</h2>
-							<img src={bannSec2} alt="bannerPrincipal"/>
+							<img src={bannSec2} alt="bannerSecundario"/>
 						</div>
 					</div>
 				</div>
+
+				<Productos/>
 			</div>
 		);
 	}
---
+++ src/Components/Productos.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import imgProducto from '../imagen/1.png';
+
+import "../Styles/productos.css";
+
+export default class Productos extends React.Component {
+	render() {
+		return (
+			<div className="productos">
+				<img src={imgProducto} alt="imagen producto"/>
+
+				<div className="descripcion">
+					<h3 className="nombre-producto">MANOMETRO ANALOGICO Y YO QUE SE MAS</h3>
+					<p className="id-producto">105402341054</p>
+					<p className="descripcion-producto">desctipcion 46-4</p>
+					<p className="disponibilidad">Disponible</p>
+				</div>
+
+				<div className="precio">
+					<h3 className="precio-producto">87,26$</h3>
+					<input type="text" placeholder="1" className="cantidad-productos"/>
+					<button className="comprar">Adicionar al carrito</button>
+
+				</div>
+			</div>
+		);
+	}
+}
