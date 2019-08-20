9e3a4d439b8ef9780c60cc873f073d157112ad31
aliLorentz <ajosue998@gmail.com>
2019-08-19 19:50:04 -0430

    Agregando banner Principal

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import NavBar from './NavBar.js';
+import "../Styles/Home.css";
+import bannerPrincipal from "../imagen/banner - principal.png";
+import bannSec1 from "../imagen/1.png";
+import bannSec2 from "../imagen/2.png";
 export default class Home extends React.Component {
 
 	render() {
 		return (
-			<div>
+			<div className="home">
 				<NavBar/>
-
+				<div className="banner-img">
+					<div className="principal">
+						<img src={bannerPrincipal} alt="bannerPrincipal"/>
+					</div>
+					<div className="secundario">
+						<div className="part1">
+							<h2>Nuestra gama de productos</h2>
+							<img src={bannSec1} alt="bannerPrincipal"/>
+						</div>
+						<div className="part2">
+							<h2>Nuestro alojamiento creo</h2>
+							<img src={bannSec2} alt="bannerPrincipal"/>
+						</div>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,6 +3,29 @@ import "../Styles/navBar.css";
 import Logo from "../imagen/logo.png";
 
 export default class NavBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state={
+			nav:"nav"
+		}
+	}
+
+	componentDidMount() {
+		 window.onscroll = () => this.handleAnimation(); 
+	}
+
+	handleAnimation =()=>{	
+		if (document.documentElement.scrollTop > 50){
+			this.setState({
+				nav:"nav-left"
+			})
+		}else if(document.documentElement.scrollTop<50){
+			this.setState({
+				nav:"nav"
+			})
+		}
+	};
+ 
 	render() {
 		return (
 			<div className="navBar">
@@ -16,30 +39,30 @@ export default class NavBar extends React.Component {
 					</div>
 				</div>
 
+				<div className={this.state.nav}>
+					<div className="nav-medium">
+						<div className="logo">
+								<img src={Logo} alt="logo"/>
+						</div>
 
-				<div className="nav-medium">
-					<div className="logo">
-							<img src={Logo} alt="logo"/>
+						<div className="buscador">
+							<input type="text" placeholder="Que estas buscando?"/>
+						</div>
+						<div className="registro">
+							<p>ENTRAR</p>
+							<p>REGISTRO</p>
+						</div>
 					</div>
 
-					<div className="buscador">
-						<input type="text" placeholder="Que estas buscando?"/>
-					</div>
-					<div className="registro">
-						<p>ENTRAR</p>
-						<p>REGISTRO</p>
+
+					<div className="nav-bottom">
+						<a href="">CASA</a>
+						<a href="">EQUIPAMIENTO</a>
+						<a href="">PESCA</a>
+						<a href="">HERRAMIENTAS</a>
+						<a href="">UN ECTERA XD</a>
 					</div>
 				</div>
-
-
-				<div className="nav-bottom">
-					<a href="">CASA</a>
-					<a href="">EQUIPAMIENTO</a>
-					<a href="">PESCA</a>
-					<a href="">HERRAMIENTAS</a>
-					<a href="">UN ECTERA XD</a>
-				</div>
-
 			</div>
 		);
 	}
